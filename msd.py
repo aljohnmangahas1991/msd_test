bb655648e823daaf6b517756259317fa3a7b4d95
Al John Mangahas <Al.John.Mangahas@justanalytics.com>
2018-12-05 00:21:48 +0800

    Refactored Code for msd.py

--- msd.py
+++ msd.py
@@ -3,8 +3,32 @@ from pyspark.sql import HiveContext
 from pyspark.sql.functions import count, avg
 from subprocess import call
 import os 
+import itertools
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
+
+def setUpEnvironment():
+    call(['hive', '-f',dir_path+"/hive_setup.hql"])
+
+def loadDatatoEnvironment(file):
+    call(['wget','https://chronicdata.cdc.gov/views/735e-byxc/rows.csv?accessType=DOWNLOAD','-O',file])
+    call(['hive', '-e',"LOAD DATA LOCAL INPATH '"+fileName+"' OVERWRITE INTO TABLE chronicdata"])
+
+def fetchData(context, query):
+    return context.sql(query)
+
+def filterData(data, filter_condition):
+    return data.filter(filter_condition)
+
+def groupData(data, grouped_cols):
+    return data.groupBy(grouped_cols)
+
+def aggregateData(data, func, col, alias):
+    return data.agg(func(col).alias(alias))
+
+def writeToHive(data, mode, hive_table):
+    data.write.mode(mode).saveAsTable(hive_table)
+
 
 fileName = dir_path + '/chronicdata.csv'
 
@@ -14,25 +38,27 @@ conf = (SparkConf()
          .set("spark.executor.memory", "4g"))
 sc = SparkContext(conf = conf)
 
-call(['wget','https://chronicdata.cdc.gov/views/735e-byxc/rows.csv?accessType=DOWNLOAD','-O',fileName])
-
-call(['hive', '-f',dir_path+"/hive_setup.hql"])
-
-call(['hive', '-e',"LOAD DATA LOCAL INPATH '"+fileName+"' OVERWRITE INTO TABLE chronicdata"])
-
 hc = HiveContext(sc)
 
-data = hc.sql("select * from chronicdata")
-header = data.take(1)
-cleanData = data.filter("YearStart != 'YearStart'")
+setUpEnvironment()
+loadDatatoEnvironment(fileName)
 
-b = cleanData.groupBy("question","yearstart").agg(avg("Data_Value").alias("avgDataValue"))
+data = fetchData(hc, "select * from chronicdata")
 
-b.write.mode('overwrite').saveAsTable('question1')
-b.write.mode('overwrite').json('/root/question1')
 
-nextQuestion= cleanData.filter("gender = 'Female'")
-c = nextQuestion.groupBy("question","yearstart").agg(avg("Data_Value").alias("avgDataValue"))
+rawData =data.rdd.mapPartitionsWithIndex(
+    lambda idx, it: itertools.islice(it, 1, None) if idx == 0 else it 
+)
 
-c.write.mode('overwrite').saveAsTable('question2')
-c.write.mode('overwrite').json('/root/question2')
+rawData = rawData.toDF()
+
+cleanData = filterData(rawData , "trim(age_months)!=''")
+
+questionA = aggregateData(groupData(cleanData, ["question","yearstart"]),avg, "Data_Value", "avgDataValue")
+
+writeToHive(questionA, 'overwrite', 'question1')
+
+cleanData = filterData(rawData , "gender = 'Female'")
+questionB = aggregateData(groupData(cleanData, ["question","yearstart"]),avg, "Data_Value", "avgDataValue")
+
+writeToHive(questionB, 'overwrite', 'question2')
